b273bc7bd3b21cb1a03d108d2b216ca369511a77
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2022-01-11 10:39:45 -0500

    js inheritance example

--- oop.js
+++ oop.js
@@ -1,46 +1,63 @@
-//js constructor 
-function Teacher(name){
-    //Attributes are assigned to the object using this keyword
-    this.name = name;
-    this.teach = function(){
-        console.log(`${this.name} says constructors are cool!`);
-    }
-}
-//The new keyword calls the function and returns the resulting object.
-let teacher1 = new Teacher('Ariel');
-let teacher2 = new Teacher('Lionel');
+// //js constructor 
+// function Teacher(name){
+//     //Attributes are assigned to the object using this keyword
+//     this.name = name;
+//     this.teach = function(){
+//         console.log(`${this.name} says constructors are cool!`);
+//     }
+// }
+// //The new keyword calls the function and returns the resulting object.
+// let teacher1 = new Teacher('Ariel');
+// let teacher2 = new Teacher('Lionel');
 
-teacher1.teach();
-teacher2.teach();
+// teacher1.teach();
+// teacher2.teach();
 
-//Constructor Exersie Answer
-function Particle(startX, startY) {
-    //attributes
-    this.x = startX;
-    this.y = startY;
-    velocity = {x: 0, y: 0};
-}
+// //Constructor Exersie Answer
+// function Particle(startX, startY) {
+//     //attributes
+//     this.x = startX;
+//     this.y = startY;
+//     velocity = {x: 0, y: 0};
+// }
 
-var particles = [];
-for(var i = 0; i < 100; i++) {
-    particles.push(new Particle(i, Math.floor(Math.random()*500)));
-}
+// var particles = [];
+// for(var i = 0; i < 100; i++) {
+//     particles.push(new Particle(i, Math.floor(Math.random()*500)));
+// }
 
-console.log(particles);
+// console.log(particles);
 
-//INHERITANCE
+// //INHERITANCE
 
-//constructor function
-function Teacher(name){
-    this.name = name;
-    sleep = function(){
-        console.log(`night night`);
-    }
+// //constructor function
+// function Teacher(name){
+//     this.name = name;
+//     sleep = function(){
+//         console.log(`night night`);
+//     }
+// }
+
+// Teacher.prototype = {
+//     teach: function(){
+//         console.log(`Prototypes are important says, ${this.name}`);
+//     }
+// }
+
+function Teacher(){
+    //look, I'm empty!    
 }
 
 Teacher.prototype = {
-    teach: function(){
-        console.log(`Prototypes are important says, ${this.name}`);
-    }
+    disposition: 'chaotic evil'
 }
+
+let shane = new Teacher();
+let assaf = new Teacher();
+console.log(shane.disposition, assaf.disposition) //evil, evil
+
+//reassigning the value of disposition
+Teacher.prototype.disposition = 'happy';
+console.log(shane.disposition, assaf.disposition) //happy, happy
+
 
